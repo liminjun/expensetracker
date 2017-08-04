4f4f2a09149194f44319882ca3306fdfdcd4126e
liminjun <v-leeli@hotmail.com>
2017-08-04 09:37:59 +0800

    feat:user can edit and delete the category.

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,27 @@ var functions = require('firebase-functions');
 // // Start writing Firebase Functions
 // // https://firebase.google.com/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+})
+
+exports.addMessage=functions.https.onRequest((req,res)=>{
+    const original=req.query.text;
+
+    admin.database().ref('/messages').push({original:original}).then(snapshot=>{
+        res.redirect(303,snapshot.ref);
+    });
+});
+
+exports.makeUppercase=functions.database.ref('/messages/{pushId}/original')
+ .onWrite(event=>{
+     const original=event.data.val();
+     console.log("Uppercasing",event.params.pushId,original);
+
+     const uppercase=original.toUpperCase();
+     return event.data.ref.parent.child('uppercase').set(uppercase);
+ });
--- public/categories/categories.js
+++ public/categories/categories.js
@@ -6,35 +6,43 @@ angular.module('app')
         //ctrl.categories=$firebaseArray(query);
         ctrl.categories = categories;
 
-
+        ctrl.isEditd = false;
+        ctrl.category = {};
 
         ctrl.createNewCategory = function () {
-            
-            ctrl.categories.$add({ name: ctrl.newCategoryName }).then(function (ref) {
-                var id = ref.key;
-                console.log(id);
-                
-            }, function () {
-                alert("添加分类失败!");
-            });
-            ctrl.newCategoryName = '';
-        }
-    }])
-    .component('categoryList', {
-        templateUrl: '/categories/categories.html',
-        bindings: {
-            categories: '='
-        },
-        controller: function ($firebaseObject, fbRef) {
-            var ctrl = this;
 
-            ctrl.createNewCategory = function () {
-                ctrl.categories.$add({ name: ctrl.newCategoryName }).then(function () {
+            if (!ctrl.isEditd) {
+                ctrl.categories.$add({
+                    name: ctrl.newCategoryName,
+                    description: ctrl.newCategoryDescription,
+                    date: new Date().toJSON()
+                }).then(function (ref) {
+                    var id = ref.key;
+                    console.log(id);
 
                 }, function () {
-
+                    alert("添加分类失败!");
                 });
-                ctrl.newCategoryName = '';
+            } else {
+                //编辑
+                ctrl.category.name = ctrl.newCategoryName;
+                ctrl.category.description = ctrl.newCategoryDescription;
+                ctrl.categories.$save(ctrl.category);
             }
+            ctrl.newCategoryName = '';
+            ctrl.newCategoryDescription = '';
+        };
+        ctrl.editCategory = function (category) {
+            ctrl.isEditd = true;
+            ctrl.category = category;
+            ctrl.newCategoryName = category.name;
+            ctrl.newCategoryDescription = category.description;
         }
-    });
+
+
+
+
+        ctrl.deleteCategory = function (category) {
+            ctrl.categories.$remove(category);
+        }
+    }]);
